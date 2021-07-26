03267039881c4d02d4b32c6133b5e08f99f124b6
ramyadav10 <ramyadav.now@gmail.com>
2021-07-26 22:22:21 +0530

    Deleting the numbers

---
+++ SingleLinkedListDelete.java
@@ -0,0 +1,65 @@
+package com.practice;
+import com.practice.SingleLinkedListInsert.Node;
+
+public class SingleLinkedListDelete {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SingleLinkedListDelete sl=new SingleLinkedListDelete();
+		sl.addNode(56);
+		sl.addNode(70);
+		sl.insertAt(1,30);
+		sl.deleteAtFirst();
+		sl.display();
+	}
+
+public class Node{
+	int data;
+	Node next;
+	
+	public Node(int d) {
+		this.data=d;
+		this.next=null;
+	}	
+}
+	Node head;
+	public void addNode(int d) {
+		Node node=new Node(d);
+		if(head==null) {
+			head=node;
+		}else {
+			Node n=head;
+			while(n.next!=null) {
+				n=n.next;
+			}
+			n.next=node;
+		}
+		
+	}
+	
+	public void display() {
+		Node temp=head;
+		while(temp.next!=null) {
+			System.out.println(temp.data);
+			temp=temp.next;
+		}
+		System.out.println(temp.data);
+	}
+	
+	public void insertAt(int index, int d) {
+		Node node=new Node(d);
+		Node n=head;
+		for(int i=0;i<index-1;i++) {
+			n=n.next;
+		}
+		node.next=n.next;
+		n.next=node;
+	}	
+	public void deleteAtFirst() {
+		
+		if (head == null){
+        }else {
+            head = head.next;
+        }
+	}
+}
